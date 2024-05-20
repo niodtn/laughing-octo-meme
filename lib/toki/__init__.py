0de9909406dbacf09f4b923b690bc6aaa486accf
ipegte93 <ipegte93@gmail.com>
2024-05-21 06:41:44 +0900

    Remove about manatoki

--- lib/toki/__init__.py
+++ lib/toki/__init__.py
@@ -1,6 +1,5 @@
 from lib import path
 from lib.toki.booktoki import booktoki
-from lib.toki.manatoki import manatoki
 from lib.toki.newtoki import newtoki
 
 
@@ -8,5 +7,4 @@ def run():
     with open(path.DATA / "toki" / "number.txt", "r", encoding="utf-8") as f:
         number = int(f.read())
         booktoki(number)
-        # manatoki(number)
         newtoki(number)
